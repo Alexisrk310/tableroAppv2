2e4a716443eeb3d70cd267060591c2fff5b1e3d1
Alexis <alexisrk310@gmail.com>
2021-07-12 17:06:37 -0500

    main tableroapp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import TableroApp from './TableroApp';
+import TableroApp from './TableroApp';
 import '../src/styles/styles.scss';
-import { Home } from './components/Home';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<TableroApp />, document.getElementById('root'));
